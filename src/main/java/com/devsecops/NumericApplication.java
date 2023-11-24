6e3a6da34ce441efb733515c278ae82f10ab2b7a
root <ee_deepakkumar@rediffmail.com>
2023-11-24 12:43:25 +0000

    restored numApp file

--- src/main/java/com/devsecops/NumericApplication.java
+++ src/main/java/com/devsecops/NumericApplication.java
@@ -9,10 +9,4 @@ public class NumericApplication {
         public static void main(String[] args) {
                 SpringApplication.run(NumericApplication.class, args);
         }
-
-        @Test
-        public void incrementByOneMessage() throws Exception {
-                this.mockMvc.perform(get("/increment/50")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string("51"));
-        }
 }
