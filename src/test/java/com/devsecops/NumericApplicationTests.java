5616fe501060e530d7aedc5944a070869ca30f65
root <ee_deepakkumar@rediffmail.com>
2023-11-24 13:23:44 +0000

    synt1

--- src/test/java/com/devsecops/NumericApplicationTests.java
+++ src/test/java/com/devsecops/NumericApplicationTests.java
@@ -37,9 +37,10 @@ public class NumericApplicationTests {
     @Test
     public void smallerThanOrEqualToFiftyMessage() throws Exception {
         this.mockMvc.perform(get("/compare/50")).andDo(print()).andExpect(status().isOk())
-        .andExpect(content().string("Smaller than or equal to 50"));
+                .andExpect(content().string("Smaller than or equal to 50"));
     }
 
+    
     @Test
     public void greaterThanFiftyMessage() throws Exception {
         this.mockMvc.perform(get("/compare/51")).andDo(print()).andExpect(status().isOk())
@@ -48,15 +49,8 @@ public class NumericApplicationTests {
 
     @Test
     public void welcomeMessage() throws Exception {
-        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
-        .andExpect(content().string("Kubernetes DevSecOps"));
-    }
-
-    @Test
-    public void incrementByOneMessage() throws Exception {
-        this.mockMvc.perform(get("/increment/50")).andDo(print()).andExpect(status().isOk())
-        .andExpect(content().string("51"));
+         this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk());
+                .andExpect(content().string("Kubernetes DevSecOps"));
     }
 
 }
-
