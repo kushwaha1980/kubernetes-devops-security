e9ddd18613f6ed1298341d25f79539c1bb79fda1
root <ee_deepakkumar@rediffmail.com>
2023-11-25 15:11:47 +0000

    restored file back controller file

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -1,6 +1,8 @@
 package com.devsecops;
 
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
