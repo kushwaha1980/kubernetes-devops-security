87ef3a413048de60073603c22522c89d78322053
root <ee_deepakkumar@rediffmail.com>
2023-11-28 13:34:12 +0000

    fixing bash script23

--- integration-test.sh
+++ integration-test.sh
@@ -5,13 +5,15 @@ sleep 5s
 PORT=$(kubectl -n default get svc ${serviceName} -o json | jq .spec.ports[].nodePort)
 
 echo $PORT
-echo $applicationURL:$PORT/$applicationURI
+echo $applicationURL:$PORT$applicationURI
 
 if [[ ! -z "$PORT" ]];
 then
     response=$(curl -s $applicationURL:$PORT$applicationURI)
     http_code=$(curl -s -o /dev/null -w "%{http_code}" $applicationURL:$PORT$applicationURI)
     echo $response
+    echo $http_code
+    
     if [[ "$response" == 100 ]];
     then
         echo "Increment test passed"
