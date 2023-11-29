39151b2399ffbc4bbb046f6377f0d4979a44a07e
root <ee_deepakkumar@rediffmail.com>
2023-11-29 14:40:16 +0000

    modify zap scr

--- zap.sh
+++ zap.sh
@@ -2,19 +2,19 @@
 
 echo "$(kubectl -n default get svc ${serviceName})"
 
-PORT=$(kubectl -n default get svc ${serviceName} -o json | jq .spec.ports[].nodePort)
+PORT=$(sudo kubectl -n default get svc ${serviceName} -o json | jq .spec.ports[].nodePort)
 
 # first run this
-chmod 777 $(pwd)
+sudo chmod 777 $(pwd)
 echo $(id -u):$(id -g)
 docker run -u 0 -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -r zap_report.html
 
 exit_code=$?
 
 # HTML Report
- mkdir -p owasp-zap-report
- chmod 777 owasp-zap-report
- mv zap_report.html owasp-zap-report
+sudo mkdir -p owasp-zap-report
+sudo chmod 777 owasp-zap-report
+sudo mv zap_report.html owasp-zap-report
 
 
 echo "Exit Code : $exit_code"
