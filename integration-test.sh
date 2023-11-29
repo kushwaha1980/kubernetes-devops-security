678f354e9bbb39727622e502f1a25d501317e26a
root <ee_deepakkumar@rediffmail.com>
2023-11-29 12:49:43 +0000

    adding lab integration scr

--- integration-test.sh
+++ integration-test.sh
@@ -1,39 +1,38 @@
 #!/bin/bash
 
+#integration-test.sh
+
 sleep 5s
 
-# applicationURL="http://mydevsecops.eastus.cloudapp.azure.com"
-# applicationURI="/increment/99"
-# serviceName="devsecops-svc"
 PORT=$(kubectl -n default get svc ${serviceName} -o json | jq .spec.ports[].nodePort)
 
-echo ${PORT}
-echo ${applicationURL}:${PORT}${applicationURI}
+echo $PORT
+echo $applicationURL:$PORT$applicationURI
 
-if [[ ! -z "${PORT}" ]];
+if [[ ! -z "$PORT" ]];
 then
-    response=$(curl -s ${applicationURL}:${PORT}${applicationURI})
-    http_code=$(curl -s -o /dev/null -w "%{http_code}" ${applicationURL}:${PORT}${applicationURI})
-    sleep 20s
-    echo "increment response: ${response}"
-    echo "http response: ${http_code}"
 
-    if [[ "${response}" == 100 ]];
-    then
-        echo "Increment test passed"
-    else
-        echo "Increment test failed"
-        #exit 1;
+    response=$(curl -s $applicationURL:$PORT$applicationURI)
+    http_code=$(curl -s -o /dev/null -w "%{http_code}" $applicationURL:$PORT$applicationURI)
+
+    if [[ "$response" == 100 ]];
+        then
+            echo "Increment Test Passed"
+        else
+            echo "Increment Test Failed"
+            exit 1;
     fi;
 
-    if [[ "${http_code}" == 200 ]];
-    then
-        echo "HTTP code test passed"
-    else
-        echo "HTTP code test failed"
-        #exit 1;
+    if [[ "$http_code" == 200 ]];
+        then
+            echo "HTTP Status Code Test Passed"
+        else
+            echo "HTTP Status code is not 200"
+            exit 1;
     fi;
+
 else
-    echo "service doesn't have nodeport"
-    exit 1;
+        echo "The Service does not have a NodePort"
+        exit 1;
 fi;
+
