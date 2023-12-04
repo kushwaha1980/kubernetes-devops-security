8e354c26ba11b8948ed220f3d27d83e440a5451f
root <ee_deepakkumar@rediffmail.com>
2023-12-04 07:43:34 +0000

    worker cis fixes

--- cis-node.sh
+++ cis-node.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-total_fail=$(kube-bench run --targets node --json | jq .[].total_fail)
+total_fail=$(kube-bench run --targets node --check=4.2.6 --json | jq .[].total_fail)
 
 if [[ "$total_fail" -ne 0 ]]; then
   echo "CIS Benchmark Failed node"
